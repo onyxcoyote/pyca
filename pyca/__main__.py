81e8a6f0521f5860c74958e654b684dbdfd696d9
onyxcoyote <no-reply@onyxcoyote.com>
2019-03-30 09:22:10 -0600

    add license text

--- pyca/__main__.py
+++ pyca/__main__.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python3
+'''
+    This file is part of pyca.
+
+    pyca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyca.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import looper
 import config
 import geminiAPI
 import geminiBuyDCAPostOnly
 from __init__ import GLOBAL_VARS
-#from __init__ import VARS
 
 
 import datetime
@@ -18,7 +32,7 @@ geminiBuyRule = None
 def main():
     print("start time:"+str(datetime.datetime.now()))
     
-    print("===TODO: COPYING NOTICE===")
+    print("pyca  Copyright (C) 2019  onyxcoyote.com")
     
     print("===loading config===")
 
